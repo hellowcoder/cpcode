3b5c37a6f138cdfc2679da3d5c9f4e7d71c363c6
hellowcoder <iitianias@gmail.com>
2024-12-10 13:32:27 +0530

    map learning

--- B_Replace_Character.cpp
+++ B_Replace_Character.cpp
@@ -1,49 +1,31 @@
+//pasted answer.
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int tt;
-    cin >> tt;
-    while (tt--) {
-        int length;
-        string input;
-        cin >> length >> input;
-
-        // Step 1: Calculate frequency of each character
-        map<char, int> frequency;
-        for (char c : input) {
-            frequency[c]++;
-        }
-
-        // Step 2: Find the characters with the highest and lowest frequency
-        char highestFreqChar = '\0', lowestFreqChar = '\0';
-        int highestFreq = 0, lowestFreq = INT_MAX;
-
-        for (const auto& pair : frequency) {
-            // Update highest frequency
-            if (pair.second > highestFreq) {
-                highestFreq = pair.second;
-                highestFreqChar = pair.first;
-            }
-            // Update lowest frequency
-            if (pair.second < lowestFreq) {
-                lowestFreq = pair.second;
-                lowestFreqChar = pair.first;
+string q; // 1e7
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n; cin>>n;
+        string s; cin>>s;
+        map <char, int> freq;
+        for(auto i: s) freq[i]++;
+        // for(auto i: m) cout<<i.first<<" "<<i.second<<endl;
+        set <pair<int, char>> ordering_freq;
+        for(auto i: freq) ordering_freq.insert({i.second, i.first});
+        // {1, d}, {2, b}, {2, c};
+        char mnfreq = (ordering_freq.begin())->second;
+        char mxfreq = (--ordering_freq.end())->second;
+        for(auto &i: s){
+            if(i == mnfreq){ 
+              i = mxfreq;
+               break;
             }
         }
-
-        // Step 3: Replace only one occurrence of the lowest frequency character with the highest frequency character
-        bool replaced = false;
-        for (char& c : input) {
-            if (c == lowestFreqChar && !replaced) {
-                c = highestFreqChar;
-                replaced = true;  // Make sure to replace only once
-            }
-        }
-
-        // Output the modified string
-        cout << input << endl;
+        cout<<s<<endl;
     }
-
-    return 0;
+   
 }
+

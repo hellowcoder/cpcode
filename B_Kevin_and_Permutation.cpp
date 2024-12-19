5ffc317d989e99ea4c7ae8b638b8b105e447e37c
hellowcoder <iitianias@gmail.com>
2024-12-19 22:08:27 +0530

    global round 28

---
+++ B_Kevin_and_Permutation.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define lli long long int
+#define fr(i,n) for(lli i=0;i<(n);i++)
+#define frs(i,s,n) for(lli i=s;i<(n);i++)
+#define fr1(i,n) for(lli i=1;i<=(n);i++)
+#define uint unsigned long long int
+#define srt(v) sort(v.begin(),v.end())
+#define rsrt(v) sort(v.rbegin(),v.rend())
+#define vec(v,n) vector<lli>v(n)
+#define vec2(v,n) vector<vector<lli>>v(n)
+#define mxe(v)  *max_element(v.begin(),v.end())
+#define mne(v)  *min_element(v.begin(),v.end())
+#define vin(a) for(auto &i:a) cin>>i
+#define psb(a) push_back(a)
+#define ppb pop_back()
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(v) (lli)(v.size())
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+using namespace std;
+const int MOD=1e9+7;
+
+void solve(){
+lli x,y;cin>>x>>y;vector<lli>v(x,-1);lli k=1;
+fr(i,x){
+    
+    if((i+1)%y==0){
+        v[i]=k;k++;
+    }
+}
+fr(i,x){
+   if(v[i]==-1){
+    v[i]=k;
+    k++;
+   }
+}
+fr(i,x){
+    cout<<v[i]<<" ";
+}cout<<'\n';
+}
+
+int main(){
+fastio;
+lli tt;cin>>tt;
+while(tt--){
+solve();
+}
+}

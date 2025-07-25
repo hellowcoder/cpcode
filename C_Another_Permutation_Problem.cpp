7b946dd63ff1a6873f9b7f585eec2e12c2f385d5
hellowcoder <iitianias@gmail.com>
2025-07-25 23:20:09 +0530

    self

---
+++ C_Another_Permutation_Problem.cpp
@@ -0,0 +1,68 @@
+//Author: sandeep172918
+//Date: 2025-07-25 23:10
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define lli long long int
+#define fr(i,n) for(lli i=0;i<n;i++)
+#define frs(i,a,b) for(lli i=a;i<=b;i++)
+#define rfr(i,b,a) for(lli i=b;i>=a;i--)
+#define srt(v) sort(v.begin(),v.end())
+#define rsrt(v) sort(v.rbegin(),v.rend())
+#define vec(v,n,k) vector<lli>v(n,k)
+#define vect(v) vector<lli>v
+#define vec2(v,x,y) vector<vector<lli>>v(x,vector<lli>(y));
+#define pr pair<lli,lli>
+#define take(x) lli x;cin>>x
+#define get(v,n) vec(v,n,0);frs(i,0,n-1)cin>>v[i]
+#define out(v) frs(i,0,v.size()-1)cout<<v[i]
+#define mxe(v)  *max_element(v.begin(),v.end())
+#define mne(v)  *min_element(v.begin(),v.end())
+#define psb(a) push_back(a)
+#define ppb pop_back()
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sq(x) sqrtl(x)
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define no1 cout<<"-1\n"
+#define nl cout<<"\n"
+#define ff first
+#define ss second
+#define srtp(v) sort(all(v),[](const pr& a,const pr& b){if(a.ff== b.ff)return a.ss>b.ss; return a.ff<b.ff;});
+using namespace std;
+const int MOD=1e9+7;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+//max(a,b)=(a+b+abs(a-b))/2
+//binary search lagale bete
+void solve(){
+lli n,k;cin>>n;
+//get(v,n);
+lli s=0,a=0;
+frs(i,1,n){
+    lli mx=0;
+    lli m=(i-1)*(i-1);
+    mx=max(mx,m);
+    s+=m;
+    lli ans=s;
+    frs(j,i,n){
+        ans+=j*(n-(j-i));
+        mx=max(mx,j*(n-(j-i)));
+    }
+    a=max(a,ans-mx);
+}
+cout<<a<<'\n';
+}
+
+int32_t main(){
+fastio;
+lli tt;cin>>tt;
+while(tt--){
+solve();
+}
+}
---
+++ D_Segments_Covering.cpp
@@ -0,0 +1,176 @@
+//Author: sandeep172918
+//Date: 2025-07-25 18:30
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define lli long long int
+#define fr(i,n) for(lli i=0;i<n;i++)
+#define frs(i,a,b) for(lli i=a;i<=b;i++)
+#define rfr(i,b,a) for(lli i=b;i>=a;i--)
+#define srt(v) sort(v.begin(),v.end())
+#define rsrt(v) sort(v.rbegin(),v.rend())
+#define vec(v,n,k) vector<lli>v(n,k)
+#define vect(v) vector<lli>v
+#define vec2(v,x,y) vector<vector<lli>>v(x,vector<lli>(y));
+#define pr pair<lli,lli>
+#define take(x) lli x;cin>>x
+#define get(v,n) vec(v,n,0);frs(i,0,n-1)cin>>v[i]
+#define out(v) frs(i,0,v.size()-1)cout<<v[i]
+#define mxe(v)  *max_element(v.begin(),v.end())
+#define mne(v)  *min_element(v.begin(),v.end())
+#define psb(a) push_back(a)
+#define ppb pop_back()
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sq(x) sqrtl(x)
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define no1 cout<<"-1\n"
+#define nl cout<<"\n"
+#define ff first
+#define ss second
+#define srtp(v) sort(all(v),[](const pr& a,const pr& b){if(a.ff== b.ff)return a.ss>b.ss; return a.ff<b.ff;});
+using namespace std;
+const int MOD=998244353;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+//max(a,b)=(a+b+abs(a-b))/2
+//binary search lagale bete
+
+struct mint{
+    int x;
+ 
+    mint (){ x = 0;}
+    mint (int32_t xx){ x = xx % MOD; if (x < 0) x += MOD;}
+    mint (long long xx){ x = xx % MOD; if (x < 0) x += MOD;}
+ 
+    int val(){
+        return x;
+    }
+    mint &operator++(){
+        x++;
+        if (x == MOD) x = 0;
+        return *this;
+    }
+    mint &operator--(){
+        if (x == 0) x = MOD;
+        x--;
+        return *this;
+    }
+    mint operator++(int32_t){
+        mint result = *this;
+        ++*this;
+        return result;
+    }
+    
+    mint operator--(int32_t){
+        mint result = *this;
+        --*this;
+        return result;
+    }
+    mint& operator+=(const mint &b){
+        x += b.x;
+        if (x >= MOD) x -= MOD;
+        return *this;
+    }
+    mint& operator-=(const mint &b){
+        x -= b.x;
+        if (x < 0) x += MOD;
+        return *this;
+    }
+    mint& operator*=(const mint &b){
+        long long z = x;
+        z *= b.x;
+        z %= MOD;
+        x = (int)z;
+        return *this;
+    }
+    mint operator+() const {
+        return *this;
+    }
+    mint operator-() const {
+        return mint() - *this;
+    }
+    mint operator/=(const mint &b){
+        return *this = *this * b.inv();
+    }
+    mint power(long long n) const {
+        mint ok = *this, r = 1;
+        while (n){
+            if (n & 1){
+                r *= ok;
+            }
+            ok *= ok;
+            n >>= 1;
+        }
+        return r;
+    }
+    mint inv() const {
+        return power(MOD - 2);
+    }
+    friend mint operator+(const mint& a, const mint& b){ return mint(a) += b;}
+    friend mint operator-(const mint& a, const mint& b){ return mint(a) -= b;}
+    friend mint operator*(const mint& a, const mint& b){ return mint(a) *= b;}
+    friend mint operator/(const mint& a, const mint& b){ return mint(a) /= b;}
+    friend bool operator==(const mint& a, const mint& b){ return a.x == b.x;}
+    friend bool operator!=(const mint& a, const mint& b){ return a.x != b.x;}
+    mint power(mint a, long long n){
+        return a.power(n);
+    }
+    friend ostream &operator<<(ostream &os, const mint &m) {
+        os << m.x;
+        return os;
+    }
+    explicit operator bool() const {
+        return x != 0;
+    }
+};
+ 
+// Remember to check MOD
+
+const int N=2e5+7;
+lli n,m;
+vector<vector<pair<lli,mint>>>v(N);
+vector<mint>dp(N);
+vector<bool>vis(N);
+mint dpp(lli x){
+    if(x>m)return 1;
+    if(vis[x])return dp[x];
+    vis[x]=true;
+    mint ans=0;
+
+    for(auto &next : v[x]){
+        lli r=next.ff;
+        mint curr=next.ss;
+        ans+=curr*dpp(r+1);
+    }
+    dp[x]=ans;
+    return ans;
+}
+
+void solve(){
+cin>>n>>m;
+
+mint val=1;
+fr(i,n){
+    lli l,r,p,q;
+    cin>>l>>r>>p>>q;
+    mint prob= mint(p)/mint(q);
+    val*=(1-prob);
+    mint w= prob/(1-prob);
+    v[l].push_back({r,w});
+}
+val*=dpp(1);
+cout<<val<<'\n';
+//cout<<mint(2)-mint(3)<<'\n';
+}
+
+int32_t main(){
+
+fastio;
+solve();
+}

5ed2f256f176e99d94ac03a9c7e7d2bf66b34093
hellowcoder <iitianias@gmail.com>
2025-10-09 14:34:30 +0530

    lcs done and dusted

--- Edit_Distance.cpp
+++ Edit_Distance.cpp
@@ -1,20 +1,24 @@
 //Author: sandeep172918
-//Date: 2025-05-06 21:56
+//Date: 2025-10-09 12:06
 
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 #define lli long long int
 #define fr(i,n) for(lli i=0;i<n;i++)
 #define frs(i,a,b) for(lli i=a;i<=b;i++)
 #define rfr(i,b,a) for(lli i=b;i>=a;i--)
 #define srt(v) sort(v.begin(),v.end())
 #define rsrt(v) sort(v.rbegin(),v.rend())
-#define vec(v,n,k) vector<lli>v(n,k)
-#define vect(v) vector<lli>v
-#define vec2(v,x,y) vector<vector<lli>>v(x,vector<lli>(y));
 #define pr pair<lli,lli>
-#define take(x) lli x;cin>>x
-#define get(v,n) vec(v,n,0);frs(i,0,n-1)cin>>v[i]
-#define out(v,n) frs(i,0,n-1)cout<<v[i]
+#define vll vector<lli>
+#define vbl vector<bool>
+#define vpr vector<pr>
+#define vvll vector<vector<lli>>
+#define get(v,n) vll v(n);fr(i,n)cin>>v[i]
+#define ff first
+#define ss second
+#define bitc(x) __builtin_popcountll(x)
 #define mxe(v)  *max_element(v.begin(),v.end())
 #define mne(v)  *min_element(v.begin(),v.end())
 #define psb(a) push_back(a)
@@ -23,35 +27,50 @@
 #define rall(v) v.rbegin(),v.rend()
 #define sq(x) sqrtl(x)
 #define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
-#define yes cout<<
-#define no cout<<
-#define ff first
-#define ss second
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define no1 cout<<"-1\n"
+#define nl cout<<"\n"
+#define out(v) fr(i,v.size())cout<<v[i]<<" ";nl
+#define srtp(v) sort(all(v),[](const pr& a,const pr& b){if(a.ff== b.ff)return a.ss>b.ss; return a.ff<b.ff;});
 using namespace std;
 const int MOD=1e9+7;
-
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
 void solve(){
-string s,k;cin>>s>>k;
-map<char,lli>m,a;
-fr(i,k.size()){
-    m[k[i]]++;
-}
-lli c=0;
-fr(i,s.size()){
-  a[s[i]]++;
-}
-fr(i,s.size()){
-    if(m[s[i]]){
-        lli k=min(m[s[i]],a[s[i]]);
-        c+=k;
-        m[s[i]]-=k;
-        a[s[i]]-=k;
+lli n,m;
+string a,b;cin>>a>>b;
+n=a.size();
+m=b.size();
+vvll dp(n+1,vll(m+1));
+fr(i,n+1)dp[i][0]=i; //deleting all char
+fr(i,m+1)dp[0][i]=i;
+
+frs(i,1,n){
+    frs(j,1,m){
+        if(a[i-1]==b[j-1]){
+          dp[i][j]=dp[i-1][j-1];
+        }else{
+          dp[i][j]=min({
+             dp[i-1][j]+1, // is j wale ko delete kr rahe mtlb uske phle thik tha
+             dp[i][j-1]+1,  //is j pe ek naya elemnt la rahe hai same i ko match krane ko iske mtlb iske upar tk thik tha
+             dp[i-1][j-1]+1, 
+          });
+        }
     }
 }
-cout<<-c+k.size()<<'\n';
+cout<<dp[n][m]<<'\n';
+
+
 }
 
 int32_t main(){
 fastio;
+lli tt=1;
+
+while(tt--){
 solve();
+}
 }
--- Longest_Common_Subsequence.cpp
+++ Longest_Common_Subsequence.cpp
@@ -1,3 +1,161 @@
+// //Author: sandeep172918
+// //Date: 2025-07-27 19:57
+
+// #include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// #define lli long long int
+// #define fr(i,n) for(lli i=0;i<n;i++)
+// #define frs(i,a,b) for(lli i=a;i<=b;i++)
+// #define rfr(i,b,a) for(lli i=b;i>=a;i--)
+// #define srt(v) sort(v.begin(),v.end())
+// #define rsrt(v) sort(v.rbegin(),v.rend())
+// #define vec(v,n,k) vector<lli>v(n,k)
+// #define vect(v) vector<lli>v
+// #define vec2(v,x,y) vector<vector<lli>>v(x,vector<lli>(y,0));
+// #define pr pair<lli,lli>
+// #define take(x) lli x;cin>>x
+// #define get(v,n) vec(v,n,0);frs(i,0,n-1)cin>>v[i]
+// #define out(v) frs(i,0,v.size()-1)cout<<v[i]<<' ',nl
+// #define mxe(v)  *max_element(v.begin(),v.end())
+// #define mne(v)  *min_element(v.begin(),v.end())
+// #define psb(a) push_back(a)
+// #define ppb pop_back()
+// #define all(v) v.begin(),v.end()
+// #define rall(v) v.rbegin(),v.rend()
+// #define sq(x) sqrtl(x)
+// #define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+// #define yes cout<<"YES\n"
+// #define no cout<<"NO\n"
+// #define no1 cout<<"-1\n"
+// #define nl cout<<"\n"
+// #define ff first
+// #define ss second
+// #define srtp(v) sort(all(v),[](const pr& a,const pr& b){if(a.ff== b.ff)return a.ss>b.ss; return a.ff<b.ff;});
+// using namespace std;
+// const int MOD=1e9+7;
+// using namespace __gnu_pbds;
+// template <typename T>
+// using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// //max(a,b)=(a+b+abs(a-b))/2
+// //binary search lagale bete
+// vector<vector<lli>>dp;
+// vector<vector<lli>>dpr;
+// lli n,m;
+// vector<lli>a,b;
+// map<pr,set<vector<lli>>>memo;
+
+
+// lli lcs_iter(){ //iterative approach
+// frs(i,1,n){
+//    frs(j,1,m){
+//       if(a[i-1]==b[j-1])dp[i][j]=1+dp[i-1][j-1];
+//       else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+//    }
+// }
+// return dp[n][m];
+// }
+
+// lli lcs_rec(lli i,lli j){ //recurssive approach
+//          if(i==0 || j==0)return 0;
+//          if(dpr[i][j] != -1)return dpr[i][j];
+//          if(a[i-1]==b[j-1]){
+           
+//             return dpr[i][j]=lcs_rec(i-1,j-1)+1;
+//          }
+//          return dpr[i][j]=max(lcs_rec(i-1,j),lcs_rec(i,j-1));
+// }
+
+// vector<lli> print_one(){
+//   vector<lli>ans;
+//   lli i=n,j=m;
+//   while(i>0 && j>0){
+//    if(a[i-1]==b[j-1]){
+//       i--;j--;
+//       ans.psb(a[i]);
+//    }else if(dp[i-1][j]>dp[i][j-1])i--;
+//    else j--;
+//  }
+// return ans;
+// }
+
+// set<vector<lli>> print_all(lli i,lli j){
+//    if(i==0 || j==0){
+//       return {{}};
+//    }
+//    if(memo.count({i,j}))return memo[{i,j}];
+    
+//    set<vector<lli>>result;
+//    if(a[i-1]==b[j-1]){
+//      auto temp=print_all(i-1,j-1);
+//      for(auto it:temp){
+//       it.push_back(a[i-1]);
+//       result.insert(it);
+//      }
+//    }else{
+//       if(dp[i-1][j]>=dp[i][j-1]){
+//         auto top=print_all(i-1,j);
+//         result.insert(all(top));
+//       }
+//       if(dp[i][j-1]>=dp[i-1][j]){
+//          auto left=print_all(i,j-1);
+//          result.insert(all(left));
+//       }
+//    }
+//    return memo[{i,j}]=result;
+// }
+
+
+
+
+
+// void solve(){
+// cin>>n>>m;
+// dp=vector<vector<lli>>(n+1,vector<lli>(m+1,0));
+// dpr=vector<vector<lli>>(n+1,vector<lli>(m+1,-1));
+// a=vector<lli>(n);
+// b=vector<lli>(m);
+// fr(i,n)cin>>a[i];
+// fr(i,m)cin>>b[i];
+
+// //print len by iterative
+// cout<<"len\n";
+// cout<<lcs_iter()<<'\n';
+
+// //print len by recurssive
+// cout<<"\nlen\n";
+
+// cout<<lcs_rec(n,m)<<'\n';
+
+// //print one possible lcs
+
+// vector<lli>ans=print_one();
+// cout<<"\none\n";
+// if(ans.size())reverse(all(ans));
+// fr(i,ans.size())cout<<ans[i]<<' ';
+// cout<<'\n';
+
+
+// //printing all possible lcs
+
+// set<vector<lli>> st =print_all(n,m);
+// cout<<"\nall\n";
+// for(auto &it:st){
+//    fr(i,it.size()){
+//       cout<<it[i]<<' ';
+
+//    }
+//    cout<<'\n';
+// }
+
+// }
+
+// int32_t main(){
+// fastio;
+// solve();
+// }
+
 //Author: sandeep172918
 //Date: 2025-07-27 19:57
 
@@ -16,7 +174,7 @@
 #define pr pair<lli,lli>
 #define take(x) lli x;cin>>x
 #define get(v,n) vec(v,n,0);frs(i,0,n-1)cin>>v[i]
-#define out(v) frs(i,0,v.size()-1)cout<<v[i]
+#define out(v) frs(i,0,v.size()-1)cout<<v[i]<<' ',nl
 #define mxe(v)  *max_element(v.begin(),v.end())
 #define mne(v)  *min_element(v.begin(),v.end())
 #define psb(a) push_back(a)
@@ -40,44 +198,115 @@ using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statisti
 
 //max(a,b)=(a+b+abs(a-b))/2
 //binary search lagale bete
+vector<vector<lli>>dp;
+vector<vector<lli>>dpr;
+lli n,m;
+vector<char>a,b;
+map<pr,set<vector<char>>>memo;
 
-// lli dpp(lli i,lli j,vector<lli>&a,vector<lli>&b,vector<vector<lli>>&dp){
-//         if(i<0 || j<0)return 0;
-//          if(dp[i][j] != -1)return dp[i][j];
-//          if(a[i]==b[j]){
-           
-//             return dp[i][j]=dpp(i-1,j-1,a,b,dp)+1;
-//          }
-//          return dp[i][j]=max(dpp(i-1,j,a,b,dp),dpp(i,j-1,a,b,dp));
-//     }
-void solve(){
-lli n,m;cin>>n>>m;
-get(a,n);
-get(b,m);
-vec2(dp,n+1,m+1);
-//cout<<dpp(n-1,m-1,a,b,dp);
-vect(ans);
+
+lli lcs_iter(){ //iterative approach
 frs(i,1,n){
    frs(j,1,m){
       if(a[i-1]==b[j-1])dp[i][j]=1+dp[i-1][j-1];
       else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
    }
 }
+return dp[n][m];
+}
 
-lli i=n,j=m;
-while(i>0 && j>0){
+lli lcs_rec(lli i,lli j){ //recurssive approach
+         if(i==0 || j==0)return 0;
+         if(dpr[i][j] != -1)return dpr[i][j];
+         if(a[i-1]==b[j-1]){
+           
+            return dpr[i][j]=lcs_rec(i-1,j-1)+1;
+         }
+         return dpr[i][j]=max(lcs_rec(i-1,j),lcs_rec(i,j-1));
+}
+
+vector<char> print_one(){
+  vector<char>ans;
+  lli i=n,j=m;
+  while(i>0 && j>0){
    if(a[i-1]==b[j-1]){
       i--;j--;
       ans.psb(a[i]);
    }else if(dp[i-1][j]>dp[i][j-1])i--;
    else j--;
+ }
+return ans;
 }
 
-cout<<ans.size()<<'\n';
-if(ans.size()){
-   reverse(all(ans));
-out(ans)<<" ";
+set<vector<char>> print_all(lli i,lli j){
+   if(i==0 || j==0){
+      return {{}};
+   }
+   if(memo.count({i,j}))return memo[{i,j}];
+    
+   set<vector<char>>result;
+   if(a[i-1]==b[j-1]){
+     auto temp=print_all(i-1,j-1);
+     for(auto it:temp){
+      it.push_back(a[i-1]);
+      result.insert(it);
+     }
+   }else{
+      if(dp[i-1][j]>=dp[i][j-1]){
+        auto top=print_all(i-1,j);
+        result.insert(all(top));
+      }
+      if(dp[i][j-1]>=dp[i-1][j]){
+         auto left=print_all(i,j-1);
+         result.insert(all(left));
+      }
+   }
+   return memo[{i,j}]=result;
 }
+
+
+
+
+
+void solve(){
+cin>>n>>m;
+dp=vector<vector<lli>>(n+1,vector<lli>(m+1,0));
+dpr=vector<vector<lli>>(n+1,vector<lli>(m+1,-1));
+a=vector<char>(n);
+b=vector<char>(m);
+fr(i,n)cin>>a[i];
+fr(i,m)cin>>b[i];
+
+//print len by iterative
+cout<<"len\n";
+cout<<lcs_iter()<<'\n';
+
+//print len by recurssive
+cout<<"\nlen\n";
+
+cout<<lcs_rec(n,m)<<'\n';
+
+//print one possible lcs
+
+vector<char>ans=print_one();
+cout<<"\none\n";
+if(ans.size())reverse(all(ans));
+fr(i,ans.size())cout<<ans[i]<<' ';
+cout<<'\n';
+
+
+//printing all possible lcs
+
+set<vector<char>> st =print_all(n,m);
+cout<<"\nall\n";
+for(auto &it:st){
+   fr(i,it.size()){
+      cout<<it[i]<<' ';
+
+   }
+   cout<<'\n';
+}
+
 }
 
 int32_t main(){
